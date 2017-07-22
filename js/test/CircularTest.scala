aef93a8198fc1b7632da9ffd9bf01e23a382e4ec
Owen Healy <owen@pulsetor.com>
2017-07-22 18:53:01 -0400

    meh

--- js/test/CircularTest.scala
+++ js/test/CircularTest.scala
@@ -8,15 +8,15 @@ object CircularTest extends App {
     val a = new Source[Seq[Int]](Seq())
     val b = new Source[Seq[Int]](Seq())
     
-    a.zipWithStalenessFrom(Seq()).foreachDelayed { case (aBefore, aAfter) => implicit u =>
-      val addSet = aAfter.toSet -- aBefore.toSet
-      val delSet = aBefore.toSet -- aAfter.toSet
-      b <<= (b.now filter (b => !delSet.contains(b))) ++ addSet.toSeq
-    }(b.redoObserving)
+    //a.zipWithStalenessFrom(Seq()).foreachDelayed { case (aBefore, aAfter) => implicit u =>
+      //val addSet = aAfter.toSet -- aBefore.toSet
+      //val delSet = aBefore.toSet -- aAfter.toSet
+      //b <<= (b.now filter (b => !delSet.contains(b))) ++ addSet.toSeq
+    //}(b.redoObserving)
     
     b.foreachDelayed { b => implicit u =>
-      val bSet = b.toSet
-      a <<= a.now filter bSet.contains
+      //val bSet = b.toSet
+      // a <<= a.now filter bSet.contains
     } (a.redoObserving)
     
     a.foreach(a => println(s"a = $a"))(globalObs)
